7c7614a42273bb6c0d05de22ae6b68c9d2ec42d4
Patrick <patrick.f.boyne@gmail.com>
2018-04-15 12:50:04 -0400

    add rest endpoints for searching

--- src/main/java/com/pluralsight/repository/ActivityRepository.java
+++ src/main/java/com/pluralsight/repository/ActivityRepository.java
@@ -3,6 +3,7 @@ package com.pluralsight.repository;
 import java.util.List;
 
 import com.pluralsight.model.Activity;
+import com.pluralsight.model.ActivitySearch;
 
 public interface ActivityRepository {
 
@@ -15,5 +16,9 @@ public interface ActivityRepository {
 	Activity update(Activity activity);
 
 	void delete(String activityId);
+
+	List<Activity> findByDescription(List<String> descriptions, int durationFrom, int durationTo);
+
+	List<Activity> findByConstraints(ActivitySearch search);
 
 }
--- src/test/java/com/pluralsight/client/ActivityClientTest.java
+++ src/test/java/com/pluralsight/client/ActivityClientTest.java
@@ -2,19 +2,61 @@ package com.pluralsight.client;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
 
 import com.pluralsight.model.Activity;
+import com.pluralsight.model.ActivitySearch;
+import com.pluralsight.model.ActivitySearchType;
 
 public class ActivityClientTest {
+
+	@Test
+	public void testSearchObject() {
+		ActivitySearchClient client = new ActivitySearchClient();
+		List<String> searchValues = new ArrayList<String>();
+		searchValues.add("biking");
+		searchValues.add("running");
+
+		ActivitySearch search = new ActivitySearch();
+		search.setDescriptions(searchValues);
+		search.setDurationFrom(30);
+		search.setDurationTo(55);
+		search.setSearchType(ActivitySearchType.SEARCH_BY_DESCRIPTION);
+
+		List<Activity> activities = client.search(search);
+
+		System.out.println(activities);
+		assertNotNull(activities);
+
+	}
+
+	@Test
+	public void testSearch() {
+		ActivitySearchClient client = new ActivitySearchClient();
+		String param = "description";
+		List<String> searchValues = new ArrayList<String>();
+		searchValues.add("swimming");
+		searchValues.add("running");
+
+		String secondParam = "durationFrom";
+		int durationFrom = 30;
+
+		String thirdParam = "durationTo";
+		int durationTo = 55;
+
+		List<Activity> activities = client.search(param, searchValues, secondParam, durationFrom, thirdParam,
+				durationTo);
+		System.out.println(activities);
+		assertNotNull(activities);
+	}
 
 	@Test
 	public void testDelete() {
 		ActivityClient client = new ActivityClient();
 		client.delete("8765");
-
 	}
 
 	@Test
